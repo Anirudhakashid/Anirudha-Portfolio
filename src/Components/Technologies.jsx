bed78f2d2fca3b8e6d8a2c26d9eaac07916299dd
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-06-24 15:53:23 +0530

    Enhance Technologies component header styling for improved visibility

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -62,7 +62,7 @@ function Technologies() {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 1.5 }}
-        className="my-20 text-center text-4xl"
+        className="my-20 p-2 text-center text-5xl font-bold bg-gradient-to-r from-white via-neutral-200 to-neutral-400 bg-clip-text text-transparent"
       >
         Technologies
       </motion.h2>
